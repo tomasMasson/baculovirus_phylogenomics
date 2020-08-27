30e93ca47d1d0445df809cc2bcc54258f3da35ed
tomas masson <tomasmasson0@gmail.com>
2020-08-27 13:17:33 -0300

    HMM parser now outputs a dataframe ready to ingest.

--- scripts/parse_hmmer-tab.py
+++ scripts/parse_hmmer-tab.py
@@ -4,9 +4,9 @@ from Bio import SearchIO
 import argparse
 
 
-def add_missing_orthologs(hmmer_search):
+def extract_gene_clusters(hmmer_search, reference_names):
     '''
-    Complete orthogroups using the results from a hhscan search
+    Complete orthogroups using the results from a hhmscan search
     against a HMM profiles database created with OrthoFinder
     orthogroups.
     '''
@@ -19,29 +19,45 @@ def add_missing_orthologs(hmmer_search):
     for query in search:
         # Store query specie and protein name
         query_protein = '_'.join(query.id.split('_')[3:5])
-        query_specie = 'NC_' + str(query.id.split('_')[1])
         # Retrieve the best hit (Orthogroup) in the database for the query
         try:
             hit_id = query[0].id
             # If the orthogroup doesn't exist , create it and store results
             if hit_id not in orthogroups.keys():
-                orthogroups[hit_id] = {
-                        'species': [query_specie],
-                        'proteins': [query_protein],
-                        'full_ids': [query.id]
-                        }
+                orthogroups[hit_id] = {'protein_id': [query.id]}
             # If the orthogroup exists, only append results
             elif hit_id in orthogroups.keys():
-                orthogroups[hit_id]['species'].append(query_specie)
-                orthogroups[hit_id]['proteins'].append(query_protein)
-                orthogroups[hit_id]['full_ids'].append(query.id)
+                orthogroups[hit_id]['protein_id'].append(query.id)
         # Handle queries without hits
         except IndexError:
             pass
-    # Results dict to standard output
+
+    # Create a dict containing reference gene names
+    with open(reference_names, "r") as f:
+        next(f)
+        gene_names = {line.split(",")[0]: line.rstrip().split(",")[1]
+                 for line in f}
+
+    # Rename orthogroups using the reference gene names
     for key in orthogroups:
-        print(f'{key},{",".join(orthogroups[key]["full_ids"])}')
-    return orthogroups
+        for protein in orthogroups[key]["protein_id"]:
+            # Check if any protein inside the orthogroup has a reference name
+            protein_id = "_".join(protein.split("_")[3:5])
+            if protein_id in gene_names.keys():
+                orthogroups[key]["gene"] = gene_names[protein_id]
+                break
+            else:
+                orthogroups[key]["gene"] = None
+
+    # Results to standard output
+    print("Protein,Specie,Orthogroup,Gene_Cluster")
+    for key in orthogroups:
+        for protein in orthogroups[key]["protein_id"]:
+            # Extract protein and specie identifier
+            protein_id = "_".join(protein.split("_")[3:5])
+            specie = "NC_" + "_".join(protein.split("_")[1:2])
+            gene_cluster = orthogroups[key]["gene"]
+            print(f"{protein_id},{specie},{key},{gene_cluster}")
 
 
 def main():
@@ -49,8 +65,9 @@ def main():
     description='Returns to standard output the proteins contained in each orthogroup.',
     usage='add_missing_orthologs <hmmer>')
     parser.add_argument('hmmer', type=str, help='HMMER search in tab format')
+    parser.add_argument('names', type=str, help='')
     args = parser.parse_args()
-    add_missing_orthologs(args.hmmer)
+    extract_gene_clusters(args.hmmer, args.names)
 
 
 if __name__ == '__main__':
